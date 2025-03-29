0261ccbda028022e209a0574322f39377b6eeb98
Laurentiu <ulaurentiu@gmail.com>
2025-03-29 20:07:08 +0200

    Add function for morse to string translation

--- main.py
+++ main.py
@@ -29,12 +29,23 @@ morse_code_dict = {
     ' ': '/'
 }
 
-def translate(param_message):
+def translate_string_to_morse(param_string_message):
     morse_message = ''
-    for character in param_message:
+    for character in param_string_message:
         if character in morse_code_dict:
             morse_message += character.replace(character, morse_code_dict[character])
             morse_message += ' '
     print(morse_message)
 
-translate(message)
+def translate_morse_to_string(param_morse_message):
+    string_message = ''
+    splitted_morse_message = param_morse_message.split(" ")
+    for morse_character in splitted_morse_message:
+        for key, character in morse_code_dict.items():
+            if morse_character == character:
+                string_message += key
+    print(string_message)
+
+# translate_string_to_morse(message)
+
+translate_morse_to_string(message)
