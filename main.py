77cceac7e636431f10ecf14d066cadb554ae4651
Laurentiu <ulaurentiu@gmail.com>
2025-03-29 21:02:50 +0200

    Reconfiguring files, running endlessly until pressing 0

--- main.py
+++ main.py
@@ -1,41 +1,4 @@
-from logo import LOGO
-
-print(LOGO)
-print ("Menu:\n"
-        "     Press 1 Translate from text to morse code\n"
-        "     Press 2 Translate from morse code to text\n")
-
-direction = int(input("Choose an option: "))
-message = input("Type the message:\n").upper() # input message
-
-# morse code dictionary
-morse_code_dict = {
-    # letters
-    'A': '.-', 'B': '-...', 'C': '-.-.',
-    'D': '-..', 'E': '.', 'F': '..-.',
-    'G': '--.', 'H': '....', 'I': '..',
-    'J': '.---', 'K': '-.-', 'L': '.-..',
-    'M': '--', 'N': '-.', 'O': '---',
-    'P': '.--.', 'Q': '--.-', 'R': '.-.',
-    'S': '...', 'T': '-', 'U': '..-',
-    'V': '...-', 'W': '.--', 'X': '-..-',
-    'Y': '-.--', 'Z': '--..',
-
-    # numbers
-    '0': '-----', '1': '.----', '2': '..---',
-    '3': '...--', '4': '....-', '5': '.....',
-    '6': '-....', '7': '--...', '8': '---..',
-    '9': '----.',
-
-    # special characters
-    '.': '.-.-.-', ',': '--..--', '?': '..--..',
-    '!': '-.-.--', "'": '.----.', '/': '-..-.',
-    '(': '-.--.', ')': '-.--.-', '&': '.-...',
-    ':': '---...', ';': '-.-.-.', '=': '-...-',
-    '+': '.-.-.', '-': '-....-', '_': '..--.-',
-    '"': '.-..-.', '$': '...-..-', '@': '.--.-.',
-    ' ': '/'
-}
+from elements import LOGO, MENU, morse_code_dict
 
 def translate_string_to_morse(param_string_message):
     morse_message = ''
@@ -55,9 +18,18 @@ def translate_morse_to_string(param_morse_message):
     print(string_message)
 
 
-if direction == 1:
-    translate_string_to_morse(message)
-elif direction == 2:
-    translate_morse_to_string(message)
-else:
-    print("Invalid selection")
+print(LOGO)
+while True:
+    print(MENU)
+
+    menu = int(input("Choose an option: "))
+    if menu == 0:
+        exit()
+    elif menu == 1:
+        message = input("Type the text message:\n").upper()
+        translate_string_to_morse(message)
+    elif menu == 2:
+        message = input("Type the morse message:\n")
+        translate_morse_to_string(message)
+    else:
+        print("Invalid selection")
