08a46baafd4b34eecf6e9061ea89c243ad4424a1
Laurentiu <ulaurentiu@gmail.com>
2025-03-30 09:28:08 +0300

    Code optimisation: reverse dict and eliminate replacing string operations with list operations

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from elements import LOGO, MENU, morse_code_dict
 
+# create a reverse dictionary to optimize the search
+reverse_morse_code_dict = {v: k for k, v in morse_code_dict.items()}
+
 def translate_string_to_morse(param_string_message):
-    morse_message = ''
+    morse_message = []
     for character in param_string_message:
         if character in morse_code_dict:
-            morse_message += character.replace(character, morse_code_dict[character])
-            morse_message += ' '
-    print(morse_message)
+            morse_message.append(morse_code_dict[character])
+    return ' '.join(morse_message)
 
 def translate_morse_to_string(param_morse_message):
-    string_message = ''
-    splitted_morse_message = param_morse_message.split(" ")
-    for morse_character in splitted_morse_message:
-        for key, character in morse_code_dict.items():
-            if morse_character == character:
-                string_message += key
-    print(string_message)
+    string_message = []
+    for morse_character in param_morse_message.split():
+        if morse_character in reverse_morse_code_dict:
+            string_message.append(reverse_morse_code_dict[morse_character])
+    return ''.join(string_message)
 
 
 print(LOGO)
@@ -27,9 +27,11 @@ while True:
         exit()
     elif menu == 1:
         message = input("Type the text message:\n").upper()
-        translate_string_to_morse(message)
+        print(translate_string_to_morse(message))
     elif menu == 2:
         message = input("Type the morse message:\n")
-        translate_morse_to_string(message)
+        print(translate_morse_to_string(message))
     else:
         print("Invalid selection")
+
+
